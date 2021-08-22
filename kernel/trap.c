0a0ed77a1425224bb947342be2353e2abe5f86d2
renjian2333 <2602734771@qq.com>
2021-08-22 21:02:13 +0800

    my solution

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,12 +67,43 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
+  } else if(r_scause() == 13||r_scause() == 15){
+    uint64 va=r_stval();
+    va=PGROUNDDOWN(va);
+     if(va>=p->sz) //overflow
+    {
+      p->killed=1;
+      goto end;
+    }
+
+    if(va<p->trapframe->sp) //below stack
+    {
+      p->killed=1;
+      goto end;
+    }
+
+    //ref uvmalloc
+    uint64 ka=(uint64)kalloc();
+    if(ka==0)
+    {
+      p->killed=1;
+      goto end;
+    }
+    memset((void*)ka,0,PGSIZE);
+    if(mappages(p->pagetable, va, PGSIZE, (uint64)ka, PTE_W|PTE_X|PTE_R|PTE_U) != 0)
+    {
+      kfree((void*)ka);
+      // uvmdealloc(p->pagetable, va+PGSIZE, va);
+      p->killed=1;
+      goto end;
+    }
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
     p->killed = 1;
   }
 
+end:
   if(p->killed)
     exit(-1);
 
--- kernel/vm.c
+++ kernel/vm.c
@@ -5,6 +5,9 @@
 #include "riscv.h"
 #include "defs.h"
 #include "fs.h"
+#include "spinlock.h"
+#include "proc.h"
+
 
 /*
  * the kernel's page table.
@@ -101,10 +104,31 @@ walkaddr(pagetable_t pagetable, uint64 va)
     return 0;
 
   pte = walk(pagetable, va, 0);
-  if(pte == 0)
-    return 0;
-  if((*pte & PTE_V) == 0)
-    return 0;
+  // if(pte == 0)
+  //   return 0;
+  // if((*pte & PTE_V) == 0)
+  //   return 0;
+  if(pte == 0||(*pte & PTE_V) == 0)
+  {
+    struct proc* p=myproc();
+    va=PGROUNDDOWN(va);
+    if(va>=p->sz) //overflow
+      return 0;
+    if(va<p->trapframe->sp) //below stack
+      return 0;
+
+    //ref uvmalloc
+    uint64 ka=(uint64)kalloc();
+    if(ka==0)
+      return 0;
+    memset((void*)ka,0,PGSIZE);
+    if(mappages(p->pagetable, va, PGSIZE, (uint64)ka, PTE_W|PTE_X|PTE_R|PTE_U) != 0)
+    {
+      kfree((void*)ka);
+      // uvmdealloc(p->pagetable, va+PGSIZE, va);
+      return 0;
+    }
+  }
   if((*pte & PTE_U) == 0)
     return 0;
   pa = PTE2PA(*pte);
@@ -181,9 +205,11 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
 
   for(a = va; a < va + npages*PGSIZE; a += PGSIZE){
     if((pte = walk(pagetable, a, 0)) == 0)
-      panic("uvmunmap: walk");
+      continue;
+      // panic("uvmunmap: walk");
     if((*pte & PTE_V) == 0)
-      panic("uvmunmap: not mapped");
+      continue;
+      // panic("uvmunmap: not mapped");
     if(PTE_FLAGS(*pte) == PTE_V)
       panic("uvmunmap: not a leaf");
     if(do_free){
@@ -315,9 +341,11 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
-      panic("uvmcopy: pte should exist");
+      continue;
+      // panic("uvmcopy: pte should exist");
     if((*pte & PTE_V) == 0)
-      panic("uvmcopy: page not present");
+      continue;
+      // panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
     flags = PTE_FLAGS(*pte);
     if((mem = kalloc()) == 0)
