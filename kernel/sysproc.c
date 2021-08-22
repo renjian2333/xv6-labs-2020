7dd508832d3ecb19c1f198f5702b8c2666232372
renjian2333 <2602734771@qq.com>
2021-08-22 16:37:29 +0800

    my solution

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -41,14 +41,20 @@ sys_wait(void)
 uint64
 sys_sbrk(void)
 {
-  int addr;
+  uint64 addr;
   int n;
 
   if(argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+  if(addr+n>=MAXVA)
+    return addr;
+  if(n<0){
+    uvmdealloc(myproc()->pagetable,addr,addr+n);
+  }
+  myproc()->sz+=n;
+  // if(growproc(n) < 0)
+  //   return -1;
   return addr;
 }
 
